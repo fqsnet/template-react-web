7b2b41d072fd84b53b0faa61ad0f095f05d29534
FQS <95135822@qq.com>
2024-03-16 22:48:43 +0800

    scss

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import { codeInspectorPlugin } from "code-inspector-plugin";
+import * as path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [codeInspectorPlugin({ bundler: "vite" }), react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  },
 });
