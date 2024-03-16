be281062e90ca9bbb5b5c8c938f388715a0cde76
FQS <95135822@qq.com>
2024-03-16 23:06:45 +0800

    router

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import {useBearStore} from "./store";
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { useBearStore } from "./store";
+import { NavLink, Routes, Route } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   function TestStore() {
     const bears = useBearStore((state) => state.bears);
     const increasePopulation = useBearStore((state) => state.increase);
-  
+
     return (
       <div>
         <h1>{bears} around here...</h1>
@@ -18,7 +19,24 @@ function App() {
       </div>
     );
   }
-  
+
+  function Home() {
+    return (
+      <>Home</>
+    )
+  }
+
+  function Settings() {
+    return (
+      <>Settings</>
+    )
+  }
+
+  function About() {
+    return (
+      <>About</>
+    )
+  }
 
   return (
     <>
@@ -44,8 +62,33 @@ function App() {
       </p>
 
       {TestStore()}
+
+      <header>
+        <h1>Hello World</h1>
+      </header>
+      <nav>
+        <NavLink to="">首页</NavLink>
+        <NavLink
+          to={{
+            pathname: "/settings",
+            search: "?sort=date",
+            hash: "#hash",
+          }}
+        >
+          产品
+        </NavLink>
+        <NavLink to="about">关于</NavLink>
+      </nav>
+
+      <Routes>
+      <Route path="/" element={<Home />} />
+        <Route path="/settings" element={<Settings />} />
+        <Route path="/about" element={<About />} />
+
+
+    </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
