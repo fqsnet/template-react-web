583a5d30bac3647e6dec21766fcadf69be067d24
FQS <95135822@qq.com>
2024-05-02 23:27:47 +0800

    persist

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,15 +1,12 @@
 import { create } from 'zustand'
+// import { persist } from 'zustand/middleware'
 
 interface BearState {
   bears: number
   increase: (by: number) => void
 }
 
-export const useBearStore = create<BearState>()((set) => ({
+export const useBearStore = create<BearState>()(set => ({
   bears: 0,
-  increase: (by) => set((state) => ({ bears: state.bears + by })),
+  increase: by => set(state => ({ bears: state.bears + by }))
 }))
-
-
-
-
