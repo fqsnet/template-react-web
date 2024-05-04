01658b076fa4ee65348e03f638851f732da7a34e
FQS <95135822@qq.com>
2024-05-04 10:19:17 +0800

    UnoCSS

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import { codeInspectorPlugin } from 'code-inspector-plugin'
 import * as path from 'path'
+import UnoCSS from 'unocss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [codeInspectorPlugin({ bundler: 'vite' }), react()],
+  plugins: [codeInspectorPlugin({ bundler: 'vite' }), react(), UnoCSS()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
